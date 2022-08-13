8012c58d7a700ad9646617e2c400d629355a9ac0
JJ Escandor <jj.p.escandor@gmail.com>
2022-08-13 18:17:47 -0400

    Finished Lab

--- snacks/urls.py
+++ snacks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SnackList, SnackDetail, AboutView
 
 urlpatterns = [
-    path('snack', SnackList.as_view(), name="snack_list"),
+    path('', SnackList.as_view(), name="snack_list"),
     path('<int:pk>', SnackDetail.as_view(), name='snack_detail'),
     path('about', AboutView.as_view(), name='about'),
 ]
